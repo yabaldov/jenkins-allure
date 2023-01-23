93067dbae85694c2ba1667113972f38a3f8d94a1
Alexey Baldov <alexey-baldov@yandex.ru>
2023-01-23 18:26:50 +0400

    adds test with an error

--- src/test/java/org/example/Mesto1Test.java
+++ src/test/java/org/example/Mesto1Test.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
@@ -16,6 +17,16 @@ public class Mesto1Test {
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
     @Test
